24ba918bea35b75e02c03c52ba2a5021cb47f71a
ayush <ayushkumarsingh97@gmail.com>
2016-06-23 19:34:57 +0530

    data vectorized

--- spam/spam.py
+++ spam/spam.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
-from sklearn.feature_extraction.text import TfidVectorizer
-from sklearn.linear_model.logistic import logisticRegression
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model.logistic import LogisticRegression
 from sklearn.cross_validation import train_test_split, cross_val_score
 
 df = pd.read_csv('SMSSpamCollection',delimiter='\t',header=None)
@@ -11,3 +11,14 @@ df = pd.read_csv('SMSSpamCollection',delimiter='\t',header=None)
 
 print "Number of spam messages", df[df[0]=='spam'][0].count()
 print "Number of ham messages", df[df[0]=='ham'][0].count()
+
+
+print type(df[0])
+X_train_raw,X_test_raw,Y_train,Y_test =  train_test_split(df[1],df[0])
+
+print X_train_raw
+print Y_train
+
+vectorizer = TfidfVectorizer()
+X_train =vectorizer.fit_transform(X_train_raw)
+X_test = vectorizer.transform(X_test_raw)
