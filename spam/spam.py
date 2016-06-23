9ccdd40b6b82a9bf351a14fb27cecdc30a34a97e
ayush <ayushkumarsingh97@gmail.com>
2016-06-23 19:55:48 +0530

    predicted

--- spam/spam.py
+++ spam/spam.py
@@ -13,12 +13,21 @@ print "Number of spam messages", df[df[0]=='spam'][0].count()
 print "Number of ham messages", df[df[0]=='ham'][0].count()
 
 
-print type(df[0])
+# print type(df[0])
 X_train_raw,X_test_raw,Y_train,Y_test =  train_test_split(df[1],df[0])
 
-print X_train_raw
-print Y_train
+print X_test_raw
+# print Y_train
 
 vectorizer = TfidfVectorizer()
 X_train =vectorizer.fit_transform(X_train_raw)
 X_test = vectorizer.transform(X_test_raw)
+
+classifier = LogisticRegression()
+classifier.fit(X_train,Y_train)
+predictions = classifier.predict(X_test)
+
+X_test_raw = list(X_test_raw)
+
+for i,prediction in enumerate(predictions[:5]):
+    print "Prediction :",prediction,"messages:",X_test_raw[i]
