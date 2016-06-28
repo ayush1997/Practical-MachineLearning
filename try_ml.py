64e759c186f2a667d526d1f3a56d229ce119260d
ayush <ayushkumarsingh97@gmail.com>
2016-06-28 19:00:58 +0530

    pca completed

--- try_ml.py
+++ try_ml.py
@@ -193,11 +193,28 @@ data = load_iris()
 y = data.target
 X = data.data
 
-print y
-print X
-
+# print y
+# print X
 
 pca = PCA(n_components=2)
 reduced_x = pca.fit_transform(X)
 
-print "reduced",reduced_x
+red_x,red_y = [],[]
+blue_x,blue_y = [],[]
+green_x,green_y = [],[]
+
+for i in range(len(reduced_x)):
+    if y[i] ==0:
+        red_x.append(reduced_x[i][0])
+        red_y.append(reduced_x[i][1])
+    elif y[i]==1:
+        blue_x.append(reduced_x[i][0])
+        blue_y.append(reduced_x[i][1])
+    else:
+        green_x.append(reduced_x[i][0])
+        green_y.append(reduced_x[i][1])
+
+plt.scatter(red_x,red_y,c='r',marker='x')
+plt.scatter(blue_x,blue_y,c='b',marker='D')
+plt.scatter(green_x,green_y,c='g',marker='.')
+plt.show()
